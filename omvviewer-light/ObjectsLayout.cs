66453571bd21c25909a531b26193829c8e9af901
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-08-14 15:57:08 +0000

    Start object window
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@441 1d7029ab-c5d0-4087-9bda-b3248858930d

--- omvviewer-light/ObjectsLayout.cs
+++ omvviewer-light/ObjectsLayout.cs
@@ -16,30 +16,50 @@ namespace omvviewerlight
 	{
 	
 		Gtk.ListStore store;	
-	    List<uint> avs = new List<uint>();
-	
+	    //List<uint> avs = new List<uint>();
+		Dictionary<uint,Primitive> objects= new Dictionary<uint,Primitive>(); 
+			
 		public ObjectsLayout()
 		{
 			this.Build();
-			store= new Gtk.ListStore (typeof(string),typeof(string),typeof(string),typeof(uint));
-			treeview1.AppendColumn("Name",new Gtk.CellRendererText(),"text",1);
-			treeview1.AppendColumn("Dist.",new Gtk.CellRendererText(),"text",2);
+			store= new Gtk.ListStore (typeof(string),typeof(string),typeof(uint));
+			treeview1.AppendColumn("Name",new Gtk.CellRendererText(),"text",0);
+			treeview1.AppendColumn("Desc.",new Gtk.CellRendererText(),"text",1);
+			treeview1.AppendColumn("ID",new Gtk.CellRendererText(),"text",2);
 			treeview1.Model=store;
 
 			MainClass.client.Objects.OnObjectKilled += new libsecondlife.ObjectManager.KillObjectCallback(onKillObject);
 			MainClass.client.Objects.OnObjectUpdated += new libsecondlife.ObjectManager.ObjectUpdatedCallback(onUpdate);
+			MainClass.client.Objects.OnNewPrim += new libsecondlife.ObjectManager.NewPrimCallback(onNewPrim);
+		
+		}
+		
+		
+		void onNewPrim(Simulator sim, Primitive prim,ulong RegionHandle,ushort timedilation)
+	    {
+			if(prim.ParentID!=0)
+				return;
+			
+		Gtk.Application.Invoke(delegate {										
+		
+			if(!objects.ContainsKey(prim.LocalID))
+			{
+				store.AppendValues(prim.PropertiesFamily.Name,prim.PropertiesFamily.Description,prim.LocalID);
+				objects.Add(prim.LocalID,prim);
+				}
+			});		
 		}
 		
 		void onUpdate(Simulator simulator, ObjectUpdate update,ulong regionHandle, ushort timeDilation)
 		{
 			//ObjectUpdate update;
-			if(!avs.Contains(update.LocalID))
+			if(objects.ContainsKey(update.LocalID))
 			{
 				//avs_pos[update.LocalID]=update.Position;
 				// I will assume libsl has done the business here for me and the avatar contains
 				// the details i need
 				Gtk.Application.Invoke(delegate {										
-					avs.Add(update.LocalID);
+					//avs.Add(update.LocalID);
 					//MainClass.client.
 					//store.AppendValues(update.
 					//store.Foreach(myfunc);
@@ -50,7 +70,7 @@ namespace omvviewerlight
 		bool myfunc(Gtk.TreeModel mod, Gtk.TreePath path, Gtk.TreeIter iter)
 		{
 			uint key=(uint)store.GetValue(iter,3);			
-			if(!avs.Contains(key))
+			if(!objects.ContainsKey(key))
 			{
 				store.Remove(ref iter);
 			}
@@ -64,9 +84,9 @@ namespace omvviewerlight
 		
 		void onKillObject(Simulator simulator, uint objectID)
 		{
-			if(avs.Contains(objectID))
+			if(objects.ContainsKey(objectID))
 			{
-				avs.Remove(objectID);
+				objects.Remove(objectID);
 				Gtk.Application.Invoke(delegate {						
 					store.Foreach(myfunc);
 				});
