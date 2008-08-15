a869641869f10ff1c0415aae8539b9cce5c8c5bf
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-08-15 14:07:13 +0000

    start profiles
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@447 1d7029ab-c5d0-4087-9bda-b3248858930d

---
+++ omvviewer-light/ProfileVIew.cs
@@ -0,0 +1,22 @@
+// ProfileVIew.cs created with MonoDevelop
+// User: robin at 15:15 15/08/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace omvviewerlight
+{
+	
+	
+	public partial class ProfileVIew : Gtk.Window
+	{
+		
+		public ProfileVIew() : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build();
+		}
+	}
+}
