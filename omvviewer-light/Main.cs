71a8d769304addd59d6bedf9a0bc65923fc4fd7a
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-08-20 16:18:51 +0000

    Sync with VS copy
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@500 1d7029ab-c5d0-4087-9bda-b3248858930d

--- omvviewer-light/Main.cs
+++ omvviewer-light/Main.cs
@@ -56,6 +56,7 @@ namespace omvviewerlight
                 IntPtr hWnd = GetConsoleWindow();
                 ShowWindow(hWnd, 0);
                 hiddenconsole = true;
+           
             }
             catch(Exception e)
             {
