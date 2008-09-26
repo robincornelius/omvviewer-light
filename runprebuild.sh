483b12551acc848ec88287bbbfdefa5c674af742
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-09-26 12:09:33 +0000

    add in prebuild shit
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@630 1d7029ab-c5d0-4087-9bda-b3248858930d

--- runprebuild.sh
+++ runprebuild.sh
@@ -4,3 +4,9 @@ mono Prebuild.exe /target nant
 #mono Prebuild.exe /target monodev
 mono Prebuild.exe /target vs2005
 
+echo "For fucks sake, now i have to run sed to insert the correct package references"
+echo "for nant/gmcs on linux as it prebuild does not do it correctly"
+
+cat omvviewer-light/omvviewer-light.exe.build | sed "s\</references>\</references><pkg-references> <package name=\"gtk-sharp-2.0\"/> </pkg-references>\g" > omvviewer-light/omvviewer-light.exe.build
+
+echo "Done.... good luck!"
