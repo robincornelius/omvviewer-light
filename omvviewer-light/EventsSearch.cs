713f6ff28e5b6505064ee8f5c007aa6bcccf1b73
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-09-28 15:23:07 +0000

    Import start of events search code
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@645 1d7029ab-c5d0-4087-9bda-b3248858930d

--- omvviewer-light/EventsSearch.cs
+++ omvviewer-light/EventsSearch.cs
@@ -21,19 +21,13 @@ namespace omvviewerlight
         int events_found;
 		
 		public EventsSearch()
-		{
+		
+	{
 			this.Build();
 			
-			store= new Gtk.ListStore (typeof(string),typeof(string));
+			store= new Gtk.ListStore (typeof(string));
 			
 			treeview1.AppendColumn("Evevnt Name",new Gtk.CellRendererText(),"text",0);
-			treeview1.AppendColumn("Sim        ",new Gtk.CellRendererText(),"text",1);		
-			//treeview1.AppendColumn("Trafic",new Gtk.CellRendererText(),"text",2);		
-			//treeview1.AppendColumn("Loc      ",new Gtk.CellRendererText(),"text",3);
-		
-		//	store.SetSortColumnId(3,Gtk.SortType.Ascending);
-		//	store.SetSortColumnId(2,Gtk.SortType.Ascending);
-			store.SetSortColumnId(1,Gtk.SortType.Ascending);
 			store.SetSortColumnId(0,Gtk.SortType.Ascending);
 			
 			treeview1.Model=store;
@@ -55,8 +49,6 @@ namespace omvviewerlight
 
 				foreach(OpenMetaverse.DirectoryManager.EventsSearchData anevent in matchedevents)
 				{	
-					//Vector3 pos=new Vector3(((int)anevent.GlobalX)&0x0000FF,((int)anevent.GlobalY)&0x0000FF,anevent.GlobalZ);
-
 					store.AppendValues(anevent.Name,anevent);
 				}
 			
@@ -69,16 +61,6 @@ namespace omvviewerlight
 			store.Clear();
 			this.label_info.Text="Searching..........";
             events_found = 0;
-			
-	//		OpenMetaverse.DirectoryManager.DirFindFlags flags;
-		//	flags=OpenMetaverse.DirectoryManager.DirFindFlags.NameSort;
-			
-		//	if(this.checkbutton_mature.Active==false)
-		//	flags|=OpenMetaverse.DirectoryManager.DirFindFlags.PgSimsOnly;
-				
-		//	EventCategories	cats;
-		//	cats=EventCateogries.
-				
 			
 			OpenMetaverse.Parcel.ParcelCategory pcat;
 			pcat=OpenMetaverse.Parcel.ParcelCategory.Any;
