400241c0e0e4ab02145649a7e85f865652debe23
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-10-01 14:21:19 +0000

    bump version for a test release
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@683 1d7029ab-c5d0-4087-9bda-b3248858930d

--- omvviewer-light/MainWindow.cs
+++ omvviewer-light/MainWindow.cs
@@ -91,7 +91,7 @@ public partial class MainWindow: Gtk.Window
 		this.statusbar1.PackStart(status_parcel);
 		this.statusbar1.PackStart(status_balance);
 
-		this.Title="Omvviewer-light v0.30 SVN";
+		this.Title="Omvviewer-light v0.30";
 		this.SetIconFromFile("viewericon.xpm");
 		
 		// Fuck stupid notebook tabs and monodeveop have to do it myself
