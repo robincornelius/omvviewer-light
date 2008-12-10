f393334ed222efaaadf57de26294ceb07d8546c7
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2008-12-10 13:10:39 +0000

    Update copyrights
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@887 1d7029ab-c5d0-4087-9bda-b3248858930d

--- omvviewer-light/OpenGL.cs
+++ omvviewer-light/OpenGL.cs
@@ -1,3 +1,35 @@
+/*
+
+  This file is based on the code for PrimWorkshop, part of libomv.
+  Copyright (c) 2007-2008, openmetaverse.org
+  All rights reserved.
+ 
+  Additional work to this file :-
+  Copyright (c) 2008 Robin Cornelius <robin.cornelius@gmail.com>
+ 
+  - Redistribution and use in source and binary forms, with or without 
+    modification, are permitted provided that the following conditions are met:
+ 
+  - Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+  - Neither the name of the openmetaverse.org nor the names 
+    of its contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+ 
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+  POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 // OpenGL.cs created with MonoDevelop
 // User: robin at 15:05 08/12/2008
 //
