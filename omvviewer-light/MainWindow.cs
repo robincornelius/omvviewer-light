a21eb5d2dc851d4c1fcd9101db8fa725017469d6
robin <robin@1d7029ab-c5d0-4087-9bda-b3248858930d>
2009-01-11 14:12:06 +0000

    Bump version start preparing for 0.45 release
    git-svn-id: svn://slupdate.byteme.org.uk/omvviewer-light@926 1d7029ab-c5d0-4087-9bda-b3248858930d

--- omvviewer-light/MainWindow.cs
+++ omvviewer-light/MainWindow.cs
@@ -160,7 +160,7 @@ public partial class MainWindow: Gtk.Window
 		this.statusbar1.PackStart(status_parcel);
 		this.statusbar1.PackStart(status_balance);
 
-		this.Title="omvviewerlight v0.44";
+		this.Title="omvviewer light v0.45";
 		
 	       chat=new ChatLayout();
            chat_tab_lable=this.addtabwithicon("icn_voice-pvtfocus.tga","Chat",chat);        
